67e8b4bbdc709313bdfd4002f9f7f87b3a612e7a
Sergey Rudenko <rudenkorssmac@gmail.com>
2020-05-26 16:50:06 +0300

    UI for Redux. Any library or framework can be used as a UI. store.dispatch() - used to update state. store.subscribe() - used to update the UI

--- src/index.js
+++ src/index.js
@@ -4,16 +4,25 @@ const reducer = (state = 0, action) => {
     switch (action.type) {
         case 'INC':
             return state + 1;
+        case 'DEC':
+            return state - 1;
         default:
             return state;
     }
 };
 
 const store = createStore(reducer);
-// Receive notification of changes
-store.subscribe(() => {
-    console.log(store.getState());
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch({type: 'INC'});
 });
 
-// Process new action
-store.dispatch({type: 'INC'});
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch({type: 'DEC'});
+});
+
+const update = () => {
+    document.getElementById('counter').innerHTML = store.getState();
+};
+
+store.subscribe(update);
